81c850c11155c5a1eae852b28909a0d7df7f85e3
Marcus441 <88486170+Marcus441@users.noreply.github.com>
2026-05-12 14:10:50 +1000

    feat(TaskListView): implement selection handling
    - Refactored api to expose new MoveUp and MoveDown that handle selection
    highlighting
      - MoveUp and MoveDown only move viewport when selected moves out of
        bounds

--- Services/Tui/Components/TaskListView.cs
+++ Services/Tui/Components/TaskListView.cs
@@ -3,13 +3,47 @@ namespace TaskManagerCli.Services.Tui.Components;
 using TaskManagerCli.Models;
 using TaskManagerCli.Services.Tui.Interfaces;
 
-public class TaskListView(IReadOnlyDictionary<int, ProcessStat> processes) : IScrollable
+public class TaskListView(IReadOnlyDictionary<int, ProcessStat> processes, int maxHeight) : IScrollable
 {
+    private readonly int _maxHeight = maxHeight;
     private readonly IReadOnlyDictionary<int, ProcessStat> _processes = processes;
+    private int _selectedAbsolute = 0;
 
     public int ScrollOffset { get; private set; }
-    public void ScrollUp(int lines = 1) => ScrollOffset = Math.Max(0, ScrollOffset - lines);
-    public void ScrollDown(int lines = 1) => ScrollOffset += lines;
+
+    private int SelectedRelative => _selectedAbsolute - ScrollOffset;
+    public void ScrollUp(int lines = 1)
+    {
+        ScrollOffset = Math.Max(0, ScrollOffset - lines);
+    }
+    public void ScrollDown(int lines = 1)
+    {
+        ScrollOffset = Math.Min(ScrollOffset + lines, _processes.Count - _maxHeight);
+    }
+    public void MoveUp()
+    {
+        if (_selectedAbsolute <= 0)
+        {
+            return;
+        }
+        _selectedAbsolute--;
+        if (_selectedAbsolute < ScrollOffset)
+        {
+            ScrollUp();
+        }
+    }
+    public void MoveDown()
+    {
+        if (_selectedAbsolute >= _processes.Count - 1)
+        {
+            return;
+        }
+        _selectedAbsolute++;
+        if (_selectedAbsolute == ScrollOffset + _maxHeight)
+        {
+            ScrollDown();
+        }
+    }
 
     public void Draw(ICanvas canvas)
     {
@@ -21,7 +55,14 @@ public class TaskListView(IReadOnlyDictionary<int, ProcessStat> processes) : ISc
         var row = 0;
         foreach (var proc in visible)
         {
-            canvas.DrawString(0, row++, proc.ToString());
+            if (row == SelectedRelative)
+            {
+                canvas.DrawString(0, row++, proc.ToString(), 0, 7);
+            }
+            else
+            {
+                canvas.DrawString(0, row++, proc.ToString());
+            }
         }
     }
 }
